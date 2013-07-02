41318729370825569c639b22de08af5567e2fc1d
Jtuttas <jtuttas@gmx.net>
2013-07-02 17:46:25 +0200

    Tablet

--- src/de/mmbbs/basicgame/Game.java
+++ src/de/mmbbs/basicgame/Game.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-//kommentar
+//kommentar 2
 public class Game extends Activity {
 	Leinwand l;
 	
